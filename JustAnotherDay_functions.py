95c94e172981861f99107232ce9309817de9c21d
MASTRIO <56378891+MASTRIO@users.noreply.github.com>
2020-08-14 16:12:53 +1000

    random number testing

--- JustAnotherDay_functions.py
+++ JustAnotherDay_functions.py
@@ -1,19 +1,20 @@
 import JustAnotherDay_variables
+import random
 
 def COMMAND_CHECK(str):
    "Command Input Goes Here!"
    if str == '/help':
         print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
         print('Here is a list of commands:')
-        print('/help\n/balance')
+        print('/help\n/balance\n/testing add random')
         print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
    if str == '/balance':
         print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
         print('You have', JustAnotherDay_variables.CHEESE, '🧀 in your wallet')
         print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-   if str == '/admin add 10':
+   if str == '/testing add random':
         print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-        print('10 🧀 has been added to ya wallet')
+        print('A random amount of 🧀 has been added to ya wallet')
         print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-        JustAnotherDay_variables.CHEESE = JustAnotherDay_variables.CHEESE + 10
+        JustAnotherDay_variables.CHEESE = JustAnotherDay_variables.CHEESE + random.randint(50,1500)
    return
