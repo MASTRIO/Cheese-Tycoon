cb77a92b0b88ee6c8f0eddc6bac5287f68b8e3f0
MASTRIO <56378891+MASTRIO@users.noreply.github.com>
2020-08-17 09:44:37 +1000

    blah

--- JAGAC_functions.py
+++ JAGAC_functions.py
@@ -17,6 +17,12 @@ def COMMAND_CHECK(str):
    # Inventory command
    if str == '/inventory':
         print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        if JAGAC_variables.resourceSHINY_GEMS > 0:
+             print('[💎] Shiny Gems =', JAGAC_variables.resourceSHINY_GEMS)
+        if JAGAC_variables.resourceCHEESE_JUMPER > 0:
+             print('[🥋] Cheese Jumpers =', JAGAC_variables.resourceCHEESE_JUMPER)
+        if JAGAC_variables.resourceBLUE_CHEESE > 0:
+             print('[🟦🧀] Blue Cheese =', JAGAC_variables.resourceBLUE_CHEESE)
         print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
    # Testing add random command
    if str == '/testing add random':
@@ -25,10 +31,11 @@ def COMMAND_CHECK(str):
         print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
         JAGAC_variables.CHEESE = JAGAC_variables.CHEESE + random.randint(50,1500)
    # Add 2 of every resource command
-   if str == '/testing add resources 2':
+   if str == '/testing add resources':
         print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-        print('Two of every resource has been given to you!')
+        print('A random amount of every resource has been given to you!')
         print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-        JAGAC_variables.resourceCHEESE_JUMPER = JAGAC_variables.resourceCHEESE_JUMPER + 2
-        JAGAC_variables.resourceSHINY_GEMS = JAGAC_variables.resourceSHINY_GEMS + 2
+        JAGAC_variables.resourceCHEESE_JUMPER = JAGAC_variables.resourceCHEESE_JUMPER + random.randint(3,150)
+        JAGAC_variables.resourceSHINY_GEMS = JAGAC_variables.resourceSHINY_GEMS + random.randint(3,150)
+        JAGAC_variables.resourceBLUE_CHEESE = JAGAC_variables.resourceBLUE_CHEESE + random.randint(3,150)
    return
--- JAGAC_variables.py
+++ JAGAC_variables.py
@@ -1,6 +1,7 @@
 # Currency
-CHEESE = 500
+CHEESE = 50
 
 # Resources/Materials
 resourceSHINY_GEMS = 0
 resourceCHEESE_JUMPER = 0
+resourceBLUE_CHEESE = 0
