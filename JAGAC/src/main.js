97acd61fbf568a262b6e440d51975b3ab835c41b
MASTRIO <56378891+MASTRIO@users.noreply.github.com>
2020-10-19 14:33:27 +1000

    billy

--- JAGAC/src/main.js
+++ JAGAC/src/main.js
@@ -1,11 +1,22 @@
+var vex = require('vex-js');
+vex.registerPlugin(require('vex-dialog'));
+vex.defaultOptions.className = 'vex-theme-os';
+
 // Variables
 let inputCommand = "";
 
+// Functions
 function formChanged() {
     inputCommand = document.getElementById("commandBox").value;
+    
+    document.getElementById("commandInput").innerHTML = '<form action="/index.html"><input type="text" id="commandBox" value="" placeholder="Type /help for a list of commands" style="width: 80%;"><input type="submit" onclick="formChanged()" value="Run"></form>'    ;
+}
 
-    if (inputCommand == "/test")
-    alert(inputCommand);
+function summonBilly() {
+    alert("No");
 
-    document.getElementById("airCookerRedeem").innerHTML = '<form action="/index.html"><input type="text" id="commandBox" value="" placeholder="Type /help for a list of commands" style="width: 80%;"><input type="submit" onclick="formChanged()" value="Run"></form>';
+    vex.dialog.prompt({
+        message: 'What is ur name',
+        placeholder: 'cheese'
+    })
 }
