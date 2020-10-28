08fbe8a8cde5555f85eb1f88494acbd080be4e73
MASTRIO <56378891+MASTRIO@users.noreply.github.com>
2020-10-28 22:48:44 +1000

    get blessed, lol

---
+++ blessed-test/test.js
@@ -0,0 +1,21 @@
+var box = blessed.box({
+    top: '0',
+    right: '0',
+    width: '50%',
+    height: '50%',
+    content: 'Hello {bold}world{/bold}!',
+    tags: true,
+    border: {
+      type: 'line'
+    },
+    style: {
+      fg: 'white',
+      bg: 'magenta',
+      border: {
+        fg: '#f0f0f0'
+      },
+      hover: {
+        bg: 'green'
+      }
+    }
+  });
