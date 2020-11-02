420dfa772a189ec29e8a8ac4ea2901b92521a558
MASTRIO <56378891+MASTRIO@users.noreply.github.com>
2020-11-02 13:48:46 +1000

    so

---
+++ JAGAC/suffer.js
@@ -0,0 +1,12 @@
+// Suffering is fun :)
+function suffer(boi, food) {
+    console.log("Uh oh! " + boi + " ate some " + food);
+}
+
+suffer("Lui", "marketable plushies");
+
+let lui = "die";
+
+if (lui == "die") {
+    suffer("idk", "hmmm");
+}
