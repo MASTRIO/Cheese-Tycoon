d1689b874f0f19f8f478d941155d9c6507f57a7c
MASTRIO <56378891+MASTRIO@users.noreply.github.com>
2021-02-16 12:50:10 +1000

    wow

---
+++ joy.java
@@ -0,0 +1,11 @@
+class joy {
+    public static void main(String[] args) {
+
+        boolean keyboardSpam = true;
+
+        if (keyboardSpam == true) {
+            System.out.println("I'M IN OOOOOH!");
+        }
+
+    }
+} 
